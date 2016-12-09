0b3cb5aac9699262a945ed74a4b4621ed02269f0
Anirudh <starfire1797@gmail.com>
2016-12-09 16:54:46 -0500

    Added the popup verify

--- app/src/main/java/cez/carshop/MainActivity.java
+++ app/src/main/java/cez/carshop/MainActivity.java
@@ -1,8 +1,11 @@
 package cez.carshop;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,6 +24,30 @@ public class MainActivity extends AppCompatActivity {
     public void addCar(View view)
     {
         startActivity(new Intent(this,AddCar.class));
+    }
+
+    public void viewCar(View view)
+    {
+        LayoutInflater li = LayoutInflater.from(this);
+        View promptsView = li.inflate(R.layout.prompt, null);
+
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setCancelable(false)
+                .setView(promptsView)
+                .setTitle("Please Enter the Information")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).create();
+        dialog.show();
     }
 
 }
